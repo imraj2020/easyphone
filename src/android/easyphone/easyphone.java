a87be2491b833eec4401f1e93ba749c73dc01f46
h.nicolau@gmail.com <h.nicolau@gmail.com@a3ffcbcf-8df6-6882-37f3-ea4937825f83>
2012-06-02 13:55:49 +0000

    - Added MenuManager

--- src/android/easyphone/easyphone.java
+++ src/android/easyphone/easyphone.java
@@ -22,13 +22,7 @@ public class easyphone extends Activity implements OnInitListener{
 	static public TextToSpeech mTTS = null;
     private int MY_DATA_CHECK_CODE;
     private boolean mttsloaded = false;
-	
-    private String mTitle;
-    private String[] mOptions = new String[2];
-    private Timer mTimer = null;
-    private int mCurrentOption = -1;
-    private int mCurrentCycle = 1;
-    private final int NCYCLES = 2; 
+    private MenuManager mMenu = null;
     
     /** Called when the activity is first created. */
     @Override
@@ -46,10 +40,9 @@ public class easyphone extends Activity implements OnInitListener{
         startActivityForResult(checkIntent, MY_DATA_CHECK_CODE); //wait for activity result
         
         //Get UI Elements
-        mTitle = (String) ((TextView)this.findViewById(R.id.TextView01)).getText();
-        mOptions[0] = (String) ((TextView)this.findViewById(R.id.TextView02)).getText();
-        mOptions[1] = (String) ((TextView)this.findViewById(R.id.TextView03)).getText();
-        mTimer = new Timer();
+        mMenu = new MenuManager();
+        mMenu.setTitle((String) ((TextView)this.findViewById(R.id.TextView01)).getText());
+        mMenu.addOption((String) ((TextView)this.findViewById(R.id.TextView02)).getText());
     }
     
     @Override
@@ -58,55 +51,7 @@ public class easyphone extends Activity implements OnInitListener{
     	Log.v(EASYPHONE_TAG, "easyphone.onStart()");
     	super.onStart();
     	
-    	if(mTTS == null) return;
-    	
-    	//READ TITLE
-    	readTitle();
-    	
-    	//READ OPTIONS
-    	scanOptions();
-    }
-    
-    private void readTitle()
-    {
-    	Log.v(EASYPHONE_TAG, "easyphone.readTitle()");
-    	mTTS.speak(mTitle, TextToSpeech.QUEUE_FLUSH, null);
-    }
-    
-    private void scanOptions()
-    {
-    	Log.v(EASYPHONE_TAG, "easyphone.scanOptions()");
-    	if(mOptions.length == 0) return;
-		mTimer.scheduleAtFixedRate(new TimerTask() {
-					@Override
-					public void run() {
-						//next option
-						mCurrentOption++;
-						if(mCurrentOption > 0 && mCurrentOption % mOptions.length == 0)
-						{
-							mCurrentOption = 0;
-							mCurrentCycle++;
-						}
-						
-						//read current options
-						mTTS.speak(mOptions[mCurrentOption], TextToSpeech.QUEUE_FLUSH, null);
-						
-						//if last option and cycle stop timer
-						if(mCurrentCycle == NCYCLES && mCurrentOption == mOptions.length - 1)
-						{
-							stopScanning();
-						}
-					}
-				}, 2500, 5000);
-    }
-    
-    private void stopScanning()
-    {
-    	Log.v(EASYPHONE_TAG, "easyphone.stopScanning()");
-    	mTimer.cancel();
-		mCurrentOption = -1;
-		mCurrentCycle = 1;
-		mTimer = new Timer();
+    	mMenu.startScanning(true);
     }
     
     @Override
@@ -125,22 +70,23 @@ public class easyphone extends Activity implements OnInitListener{
               }
               case MotionEvent.ACTION_UP:
               {  // finger up event, Select current option
-            	  int option = mCurrentOption;
+            	  int option = mMenu.getCurrentOption();
             	  
             	  if(option >= 0)
             	  {
             		  //is scanning, thus select option
-            		  stopScanning();
+            		  mMenu.stopScanning();
             		  selectOption(option);
+            	  }
+            	  else if(option == -1 && mMenu.isScanning())
+            	  {
+            		  mMenu.stopScanning();
+            		  selectOption(0);
             	  }
             	  else
             	  {
-            		//is not scanning, thus start scanning            		
-            		//READ TITLE
-            	    readTitle();
-            	    	
-            	    //READ OPTIONS
-            	    scanOptions();
+            		//is not scanning, thus start scanning
+            		  mMenu.startScanning(true);
             	  }
             	  
                   break;
@@ -159,12 +105,6 @@ public class easyphone extends Activity implements OnInitListener{
 	    		//Call
 	    		Intent contactList =  new Intent(getApplicationContext(), ContactList.class);
 	    		startActivity(contactList);
-	    		break;
-	    	}
-	    	case 1:
-	    	{
-	    		//Missed Calls
-	    		
 	    		break;
 	    	}
     	}
