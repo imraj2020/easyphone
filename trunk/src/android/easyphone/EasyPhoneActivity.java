c99c2e30717db7642935a4adf1e4a597700e2775
h.nicolau@gmail.com <h.nicolau@gmail.com@a3ffcbcf-8df6-6882-37f3-ea4937825f83>
2012-06-27 13:04:48 +0000

    + Common Activity

--- trunk/src/android/easyphone/EasyPhoneActivity.java
+++ trunk/src/android/easyphone/EasyPhoneActivity.java
@@ -2,33 +2,41 @@ package android.easyphone;
 
 import java.util.Timer;
 import java.util.TimerTask;
-
 import android.app.Activity;
 import android.app.KeyguardManager;
+import android.content.Context;
 import android.graphics.PixelFormat;
 import android.os.Bundle;
 import android.speech.tts.TextToSpeech;
 import android.util.Log;
 import android.view.KeyEvent;
+import android.view.LayoutInflater;
 import android.view.MotionEvent;
 import android.view.Window;
 import android.view.WindowManager;
+import android.widget.LinearLayout;
 
 public class EasyPhoneActivity extends Activity
 {
 	static public String EASYPHONE_TAG = "EasyPhone";
+	
+	// menu variables
 	protected MenuManager mMenu = null;
 	protected String mName = "";
 	protected boolean mReadMenu = true;
 	
-	//exit variables
+	// exit variables
 	private int mMenuHits = 0;
 	private boolean mHasStarted = false;
 	private int TIME_THRESHOLD = 2000; // 1s
 	private Timer mExitTimer = null;
 	
+	// window variables
+	LinearLayout mContainer = null;
+	WindowManager mWindowManager = null;
+	
 	/** Called when the activity is first created. */
-    public void onCreate(Bundle savedInstanceState, String activityName) 
+    public void onCreate(Bundle savedInstanceState, String activityName, int layoutResource) 
     {
     	mName = activityName;
     	Log.v(EASYPHONE_TAG, mName + ".onCreate()");
@@ -42,12 +50,32 @@ public class EasyPhoneActivity extends Activity
     	mMenuHits = 0;
     	mHasStarted = false;
     	mExitTimer = new Timer();
+    	
+    	// Always on top window
+        WindowManager.LayoutParams params = new WindowManager.LayoutParams( 
+        		WindowManager.LayoutParams.FLAG_FULLSCREEN, 
+        		WindowManager.LayoutParams.FLAG_FULLSCREEN,                 
+        		WindowManager.LayoutParams.TYPE_SYSTEM_ALERT,               
+        		WindowManager.LayoutParams.FLAG_WATCH_OUTSIDE_TOUCH,
+        		PixelFormat.OPAQUE);
+        mWindowManager = (WindowManager) getApplicationContext().getSystemService(Context.WINDOW_SERVICE);    
+       
+        // Create window container to be always on top
+        mContainer = new Container(this.getApplicationContext());
+        mContainer.setLayoutParams(params);
+        mContainer.setClickable(true);
+        mContainer.setFocusable(true);
+        mContainer.setFocusableInTouchMode(true);
+        
+        LayoutInflater inflater = (LayoutInflater) this.getApplicationContext().getSystemService(Context.LAYOUT_INFLATER_SERVICE);
+        inflater.inflate(layoutResource, mContainer);
+        mWindowManager.addView(mContainer, params);
     }
     
-    public void onCreate(Bundle savedInstanceState, String activityName, boolean readMenuOnStart) 
+    public void onCreate(Bundle savedInstanceState, String activityName, int layoutResource, boolean readMenuOnStart) 
     {
     	mReadMenu = readMenuOnStart;
-    	onCreate(savedInstanceState, activityName);
+    	onCreate(savedInstanceState, activityName, layoutResource);
     }
     
     @Override
@@ -77,6 +105,24 @@ public class EasyPhoneActivity extends Activity
         WindowManager.LayoutParams lp = getWindow().getAttributes();
         lp.screenBrightness = (float) 10;
         getWindow().setAttributes(lp);
+    }
+    
+    @Override
+    public void onDestroy()
+    {
+    	Log.v(EASYPHONE_TAG, mName + ".onDestroy()");
+    	
+    	Log.v(easyphone.EASYPHONE_TAG, mName + ".onDestroy()");
+    	try
+    	{
+	    	mWindowManager.removeView(mContainer);
+    	}
+    	catch(Exception e)
+    	{
+    		Log.v(easyphone.EASYPHONE_TAG, mName + "onDestroy() - EXCEPTION");
+    	}
+    	
+    	super.onDestroy();
     }
     
     @Override
@@ -148,7 +194,7 @@ public class EasyPhoneActivity extends Activity
     @Override 
     public boolean onKeyDown(int keyCode, KeyEvent event)
     {
-    	Log.v(EASYPHONE_TAG, mName + ".onKeyDown()");
+    	Log.v(EASYPHONE_TAG, mName + ".onKeyDown(): " + keyCode);
     	if(keyCode == KeyEvent.KEYCODE_BACK)
     	{
     		return super.onKeyDown(keyCode, event);
@@ -157,7 +203,7 @@ public class EasyPhoneActivity extends Activity
     	{
     		processMenuKey();
     	}
-    	else if(keyCode == KeyEvent.KEYCODE_VOLUME_UP || keyCode == KeyEvent.KEYCODE_VOLUME_UP)
+    	else if(keyCode == KeyEvent.KEYCODE_VOLUME_DOWN || keyCode == KeyEvent.KEYCODE_VOLUME_UP)
     	{
     		return false;
     	}
@@ -168,7 +214,7 @@ public class EasyPhoneActivity extends Activity
     @Override
     public void onAttachedToWindow() 
     {
-    	Log.v(EASYPHONE_TAG, mName + ".onAttachedToWindow()");
+    	/*Log.v(EASYPHONE_TAG, mName + ".onAttachedToWindow()");
     	
     	// Disable Power Button
     	KeyguardManager keyguardManager = (KeyguardManager) getSystemService(Activity.KEYGUARD_SERVICE);
@@ -176,24 +222,64 @@ public class EasyPhoneActivity extends Activity
         lock.disableKeyguard();
         
     	// Disable Home Button
-        this.getWindow().setType(WindowManager.LayoutParams.TYPE_SYSTEM_ALERT);
-       /* WindowManager.LayoutParams params = new WindowManager.LayoutParams( 
-        		WindowManager.LayoutParams.FLAG_FULLSCREEN, 
-        		WindowManager.LayoutParams.FLAG_FULLSCREEN,                 
-        		WindowManager.LayoutParams.TYPE_SYSTEM_ALERT,               
-        		WindowManager.LayoutParams.FLAG_WATCH_OUTSIDE_TOUCH,
-        		PixelFormat.OPAQUE);
-        getWindow().setAttributes(params);*/
-        
+        //this.getWindow().setType(WindowManager.LayoutParams.TYPE_SYSTEM_ALERT);*/
         super.onAttachedToWindow();
     }
     
+    // default selectOption behavior
     protected void selectOption(int option)
     {
     	Log.v(EASYPHONE_TAG, mName + ".selectOption()");
     	easyphone.mTTS.playEarcon("click", TextToSpeech.QUEUE_FLUSH, null);
     }
     
+    /* CONTAINER CLASS AND DISPATCHING METHODS */
+    
+    /* This will only be called when the container has focus. */
+    private boolean onContainerKey(KeyEvent event) {
+        int keyCode = event.getKeyCode();
+        
+        if(event.getAction() == KeyEvent.ACTION_DOWN)
+        {
+        	return onKeyDown(keyCode, event);
+        }
+        else if(event.getAction() == KeyEvent.ACTION_UP && keyCode == KeyEvent.KEYCODE_BACK)
+		{
+			onBackPressed();
+		}
+        return true;
+    }
+    
+    public boolean onContainerTouchEvent(MotionEvent event)
+    { 
+    	return onTouchEvent(event);
+    }
+    
+    // Container Class
+    private class Container extends LinearLayout {
+        public Container(Context context) {
+            super(context);
+        }
+
+        /*
+         * Need to override this to intercept the key events. Otherwise, we
+         * would attach a key listener to the container but its superclass
+         * ViewGroup gives it to the focused View instead of calling the key
+         * listener, and so we wouldn't get the events.
+         */
+        @Override
+        public boolean dispatchKeyEvent(KeyEvent event) {
+            return onContainerKey(event) ? true : super.dispatchKeyEvent(event);
+        }
+        
+        @Override
+        public boolean onTouchEvent(MotionEvent event)
+        { 
+        	return onContainerTouchEvent(event) ? true : super.onTouchEvent(event);
+        }
+    }
+    
+    // check if key combinations allow exit
     private void processMenuKey()
     {
     	mMenuHits++;
--- trunk/src/android/easyphone/IncomingCall.java
+++ trunk/src/android/easyphone/IncomingCall.java
@@ -18,37 +18,13 @@ import android.view.WindowManager;
 import android.widget.LinearLayout;
 import android.widget.TextView;
 
-public class IncomingCall extends EasyPhoneActivity{
-
-	//ViewGroup mTopView = null;
-	LinearLayout mContainer = null;
+public class IncomingCall extends EasyPhoneActivity{	
 	
 	/** Called when the activity is first created. */
     @Override
     public void onCreate(Bundle savedInstanceState) {
-        super.onCreate(savedInstanceState, "IncomingCall", false);
-        
+        super.onCreate(savedInstanceState, "IncomingCall", R.layout.incomingcall, false);
         setContentView(R.layout.incomingcall);
-        
-        /* ALWAYS ON TOP WINDOW */
-        WindowManager.LayoutParams params = new WindowManager.LayoutParams( 
-        		WindowManager.LayoutParams.FLAG_FULLSCREEN, 
-        		WindowManager.LayoutParams.FLAG_FULLSCREEN,                 
-        		WindowManager.LayoutParams.TYPE_SYSTEM_ALERT,               
-        		WindowManager.LayoutParams.FLAG_WATCH_OUTSIDE_TOUCH,
-        		PixelFormat.OPAQUE);
-        WindowManager wm = (WindowManager) getApplicationContext().getSystemService(Context.WINDOW_SERVICE);    
-       
-        // Create window container to be always on top
-        mContainer = new Container(this.getApplicationContext());
-        mContainer.setLayoutParams(params);
-        mContainer.setClickable(true);
-        mContainer.setFocusable(true);
-        mContainer.setFocusableInTouchMode(true);
-        
-        LayoutInflater inflater = (LayoutInflater) this.getApplicationContext().getSystemService(Context.LAYOUT_INFLATER_SERVICE);
-        inflater.inflate(R.layout.incomingcall, mContainer);
-        wm.addView(mContainer, params);
         
         // Register receiver to close this activity
         getApplicationContext().registerReceiver(receiver, new IntentFilter("android.easyphone.CLOSE_INCOMINGCALL_ACTIVITY"));
@@ -82,24 +58,24 @@ public class IncomingCall extends EasyPhoneActivity{
         mMenu.addOption((String) ((TextView)this.findViewById(R.id.TextView03)).getText());
     }    
     
-    /* This will only be called when the container has focus. */
-    private boolean onContainerKey(KeyEvent event) {
-        int keyCode = event.getKeyCode();
-        
-        if(event.getAction() == KeyEvent.ACTION_UP && keyCode == KeyEvent.KEYCODE_BACK)
-		{
-			super.onBackPressed();
-		}
-        return true;
-    }
-    
+    @Override
     public boolean onContainerTouchEvent(MotionEvent event)
-    { 
+    {
     	if(!mMenu.isScanning())
     	{
     		easyphone.callControl.silenceRinger();
     	}
-    	return super.onTouchEvent(event);
+    	return super.onContainerTouchEvent(event);
+    }
+    
+    @Override
+    public void onBackPressed() 
+    {
+    	if(!mMenu.isScanning())
+    	{
+    		easyphone.callControl.silenceRinger();
+    	}
+    	super.onBackPressed();
     }
     
     @Override
@@ -111,7 +87,7 @@ public class IncomingCall extends EasyPhoneActivity{
 	    	case 0:  
 	    	{
 	    		//Accept call
-	    		closeWindow();
+	    		//closeWindow();
 	    		easyphone.callControl.answerCall();
 	    		this.finish();
 	    		break;
@@ -126,23 +102,6 @@ public class IncomingCall extends EasyPhoneActivity{
     	}
     }
     
-    private void closeWindow()
-    {
-    	Log.v(easyphone.EASYPHONE_TAG, "IncomingCall.closeWindow()");
-    	try
-    	{
-	    	WindowManager wm = (WindowManager) getApplicationContext().getSystemService(Context.WINDOW_SERVICE);
-	    	//wm.removeView(mTopView);
-	    	wm.removeView(mContainer);
-    	}
-    	catch(Exception e)
-    	{
-    		Log.v(easyphone.EASYPHONE_TAG, "IncomingCall.closeWindow() - EXCEPTION");
-    		return;
-    	}
-    	
-    }
-    
     /* USED BY CALLCONTROL */
     private BroadcastReceiver receiver = new BroadcastReceiver() {
 
@@ -152,32 +111,9 @@ public class IncomingCall extends EasyPhoneActivity{
         	if(intent.getAction().equals("android.easyphone.CLOSE_INCOMINGCALL_ACTIVITY"))
         	{
         		getApplicationContext().unregisterReceiver(receiver);
-        		closeWindow();
+        		//closeWindow();
         		IncomingCall.this.finish();
         	}
         }
     };
-    
-    private class Container extends LinearLayout {
-        public Container(Context context) {
-            super(context);
-        }
-
-        /*
-         * Need to override this to intercept the key events. Otherwise, we
-         * would attach a key listener to the container but its superclass
-         * ViewGroup gives it to the focused View instead of calling the key
-         * listener, and so we wouldn't get the events.
-         */
-        @Override
-        public boolean dispatchKeyEvent(KeyEvent event) {
-            return onContainerKey(event) ? true : super.dispatchKeyEvent(event);
-        }
-        
-        @Override
-        public boolean onTouchEvent(MotionEvent event)
-        { 
-        	return onContainerTouchEvent(event) ? true : super.onTouchEvent(event);
-        }
-    }
 }
